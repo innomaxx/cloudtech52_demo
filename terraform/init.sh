48954d0066768660f5447391bee32f327dac21d8
Max Inno <maksym.hubach@gmail.com>
2022-07-05 18:33:07 +0300

    Fix init.sh

--- terraform/init.sh
+++ terraform/init.sh
@@ -2,7 +2,7 @@
 echo "====================Start====================="
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-mkdir /app && cd /app
+mkdir app && cd app
 git clone https://github.com/innomaxx/cloudtech52_demo.git .
 sudo docker compose -f "docker-compose.prod.yml" up -d
 echo "====================End====================="
