1b53312610757978c8eccdb6460708f528ab563a
Max Inno <maksym.hubach@gmail.com>
2022-07-05 18:58:18 +0300

    Return docker install to init.sh

--- terraform/init.sh
+++ terraform/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "====================Start====================="
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 mkdir app && cd app
 git clone https://github.com/innomaxx/cloudtech52_demo.git .
 sudo docker compose -f "docker-compose.prod.yml" up -d
