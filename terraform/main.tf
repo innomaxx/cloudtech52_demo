40410c005047333304a88f8df7cc649fda1da81f
Max Inno <maksym.hubach@gmail.com>
2022-07-06 12:29:25 +0300

    Terraform hide port 22

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,13 +25,6 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     protocol    = "-1"
