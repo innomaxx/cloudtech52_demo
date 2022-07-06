8dee5ed19540887c99984a043dcadf1ab2393437
Max Inno <maksym.hubach@gmail.com>
2022-07-06 18:41:28 +0300

    Hide credentials to env vars

--- terraform/init.sh
+++ terraform/init.sh
@@ -4,5 +4,6 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 mkdir app && cd app
 git clone https://github.com/innomaxx/cloudtech52_demo.git .
+git checkout feature/hide_creds
 sudo docker compose -f "docker-compose.prod.yml" up -d
 echo "====================End====================="
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,16 @@ resource "aws_instance" "app_server" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   user_data              = file("init.sh")
 
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p app",
+      "echo DB_HOST=${var.db_host} >> app/.env",
+      "echo DB_NAME=${var.db_name} >> app/.env",
+      "echo DB_USER=${var.db_user} >> app/.env",
+      "echo DB_PASS=${var.db_pass} >> app/.env"
+    ]
+  }
+
   tags = {
     Name = "instance"
   }
