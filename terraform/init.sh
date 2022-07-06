ca09a3e1763b3cc871788eea8ea1425a7cd9ed48
Max Inno <maksym.hubach@gmail.com>
2022-07-06 15:52:53 +0300

    Add checkout to branch feature/nginx_mon

--- terraform/init.sh
+++ terraform/init.sh
@@ -4,5 +4,6 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 mkdir app && cd app
 git clone https://github.com/innomaxx/cloudtech52_demo.git .
+git checkout feature/nginx_mon
 sudo docker compose -f "docker-compose.prod.yml" up -d
 echo "====================End====================="
